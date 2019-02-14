2e2576fdb32c3a7d23f9ff35497ea63ea65e9f98
Marcin <komar86311@gmail.com>
2019-02-14 22:53:20 +0100

    executable v1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
 os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
 os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
 
-executables=[cx_Freeze.Executable("mygame.py", icon="icon.ico")]
+executables=[cx_Freeze.Executable("mygame.py", icon="icon.ico", base = "Win32GUI")]
 cx_Freeze.setup(
     name="Kostchevskys PvP Game",
     options={"build_exe": {"packages":["pygame", "os", "sys"], "include_files":[
